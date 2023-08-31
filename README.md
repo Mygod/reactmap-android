8d317e5e89c408334b2d57df71f648f3b862fbb3
Mygod <contact-git@mygod.be>
2023-08-31 01:04:05 -0400

    v0.4.0

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ Use ReactMap as an Android app with battery improvements, including:
 
 Other features:
 
+* See map even behind your status bar and navigation bar!
 * More conveniently refresh the webpage when fullscreen.
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "be.mygod.reactmap"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10
-        versionName = "0.3.0"
+        versionCode = 11
+        versionName = "0.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
