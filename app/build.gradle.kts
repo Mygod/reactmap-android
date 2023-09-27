82f998b534a858bf474c535568a2f1b03af59c1f
Mygod <contact-git@mygod.be>
2023-09-27 17:21:34 -0400

    v0.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "be.mygod.reactmap"
         minSdk = 26
         targetSdk = 34
-        versionCode = 52
-        versionName = "0.5.1"
+        versionCode = 53
+        versionName = "0.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
